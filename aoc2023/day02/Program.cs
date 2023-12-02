20db3a14bc3c1ea7111cc0b532534a72c5925b80
Dmitry Shechtman <damnian@gmail.com>
2023-12-02 10:56:55 +0200

    Add day02

---
+++ aoc2023/day02/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace aoc.aoc2023.day02
+{
+    class Program
+    {
+        record Game(int Id, Dictionary<string, int>[] Sets);
+
+        private static readonly Dictionary<string, int> Cubes = new()
+            { { "red", 12 }, { "green", 13 }, { "blue", 14 } };
+
+        static void Main(string[] args)
+        {
+            var games = Parse(args[0]);
+            Console.WriteLine(Part1(games));
+            Console.WriteLine(Part2(games));
+        }
+
+        private static int Part1(Game[] games) =>
+            games.Sum(game => game.Sets.All(set =>
+                !set.Any(kvp => kvp.Value > Cubes[kvp.Key])) ? game.Id : 0);
+
+        private static long Part2(Game[] games) =>
+            games.Sum(game => Cubes.Keys.Product(key =>
+                game.Sets.Max(set => set.GetValueOrDefault(key))));
+
+        private static Game[] Parse(string path) =>
+            File.ReadAllLines(path)
+                .Select(s => s.Split(": "))
+                .Select(tt => new Game(
+                    int.Parse(tt[0].Split(' ')[1]),
+                    tt[1].Split("; ")
+                        .Select(t => t.Split(", ")
+                            .Select(u => u.Split(' '))
+                            .ToDictionary(vv => vv[1], vv => int.Parse(vv[0])))
+                        .ToArray()))
+                .ToArray();
+    }
+}
