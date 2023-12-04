b8373cc49ab0e2d204e0e449d3371c9ab7452ca7
Dmitry Shechtman <damnian@gmail.com>
2023-12-04 09:24:02 +0200

    Add day04

---
+++ aoc2023/day04/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace aoc.aoc2023.day04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Parse(args[0]);
+            Console.WriteLine(Part1(input));
+            Console.WriteLine(Part2(input));
+        }
+
+        private static double Part1(int[] input) =>
+            input.Sum(c => c > 0 ? 1 << (c - 1) : 0);
+
+        private static int Part2(int[] input)
+        {
+            var output = Enumerable.Repeat(1, input.Length).ToArray();
+            for (int i = 0; i < input.Length; ++i)
+                for (int j = 1; j <= input[i] && i + j < input.Length; ++j)
+                    output[i + j] += output[i];
+            return output.Sum();
+        }
+
+        private static int[] Parse(string path) =>
+            File.ReadAllLines(path)
+                .Select(ParseOne)
+                .Select(tt => tt[0].Intersect(tt[1]).Count())
+                .ToArray();
+
+        private static string[][] ParseOne(string s) =>
+            s.Split(": ")[1].Split(" | ")
+                .Select(s => s.Split(' ', StringSplitOptions.RemoveEmptyEntries))
+                .ToArray();
+    }
+}
