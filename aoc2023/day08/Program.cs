c7788a555b7c5986a9a4609cb8b9d643e57c35ca
Dmitry Shechtman <damnian@gmail.com>
2023-12-08 13:43:15 +0200

    Translate day08 to Ukrainian

--- aoc2023/day08/Program.cs
+++ aoc2023/day08/Program.cs
@@ -3,58 +3,113 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
+using ціле   = System.Int32;
+using довге  = System.Int64;
+using рядок  = System.String;
+using літера = System.Char;
+
 namespace aoc.aoc2023.day08
 {
-    class Program
+    using Мапа  = Dictionary<рядок, рядок[]>;
+    using Кроки = HashSet<(ціле, ціле)>;
+
+    static class Program
     {
-        static void Main(string[] args)
+        static void Main(рядок[] арг)
         {
-            Parse(args[0], out var dirs, out var map);
-            Console.WriteLine(Part1(dirs, map));
-            Console.WriteLine(Part2(dirs, map));
+            Розбір(арг[0], out ціле[] напр, out Мапа мапа);
+            Console.WriteLine(Частина1(напр, мапа));
+            Console.WriteLine(Частина2(напр, мапа));
         }
 
-        private static int Part1(int[] dirs, Dictionary<string, string[]> map)
+        private static ціле Частина1(ціле[] напр, Мапа мапа)
         {
-            int step = 0;
-            for (var node = "AAA"; node != "ZZZ"; ++step)
-                node = map[node][dirs[step % dirs.Length]];
-            return step;
+            ціле крок = 0;
+            for (рядок вузол = "AAA"; вузол != "ZZZ"; ++крок)
+                вузол = мапа[вузол][напр[крок % напр.Довжина()]];
+            return крок;
         }
 
-        private static long Part2(int[] dirs, Dictionary<string, string[]> map)
+        private static довге Частина2(ціле[] напр, Мапа мапа)
         {
-            var keys = map.Keys.ToArray();
-            var values = map.Values
-                .Select(t => t.Select(t => Array.IndexOf(keys, t)).ToArray())
-                .ToArray();
-            return keys
-                .Select((s, i) => s[^1] == 'A' ? GetCycle(i, dirs, values) : 1)
-                .Aggregate(Lcm);
+            рядок[] вузли = мапа.Ключі().НаМасив();
+            ціле[][] знач = мапа.Значення()
+                .Вибір(знач => знач.Вибір(вузол => вузли.Індекс(вузол)).НаМасив())
+                .НаМасив();
+            return вузли
+                .Вибір((вузол, ліч) => вузол[^1] == 'A' ? ПошукЦиклу(ліч, напр, знач) : 1)
+                .Агрегат(Нск);
         }
 
-        private static long GetCycle(int start, int[] dirs, int[][] map)
+        private static довге ПошукЦиклу(ціле початок, ціле[] напр, ціле[][] мапа)
         {
-            HashSet<(int, int)> steps = new();
-            var curr = (node: start, step: 0);
-            while (steps.Add(curr))
-                curr = (map[curr.node][dirs[curr.step]], (curr.step + 1) % dirs.Length);
-            return steps.Count - curr.step;
+            Кроки кроки = new();
+            (ціле вузол, ціле крок) потч = (початок, 0);
+            while (кроки.Add(потч))
+                потч = (мапа[потч.вузол][напр[потч.крок]], (потч.крок + 1) % напр.Довжина());
+            return кроки.Кількість() - потч.крок;
         }
 
-        private static long Lcm(long a, long b) =>
-            a / Gcd(a, b) * b;
+        private static довге Нск(довге а, довге б) =>
+            а / Нсд(а, б) * б;
 
-        private static long Gcd(long a, long b) =>
-            b == 0 ? a : Gcd(b, a % b);
+        private static довге Нсд(довге а, довге б) =>
+            б == 0 ? а : Нсд(б, а % б);
 
-        private static void Parse(string path, out int[] dirs, out Dictionary<string, string[]> map)
+        private static void Розбір(рядок шлях, out ціле[] напр, out Мапа мапа)
         {
-            var ss = File.ReadAllText(path).Split("\n\n");
-            dirs = ss[0].Select(t => "LR".IndexOf(t)).ToArray();
-            map = ss[1].Trim().Split('\n')
-                .Select(t => t.Split(" = "))
-                .ToDictionary(tt => tt[0], tt => tt[1][1..^1].Split(", "));
+            рядок[] рядки = File.ReadAllText(шлях).Розділ("\n\n");
+            напр = рядки[0].Вибір(літ => "LR".Індекс(літ)).НаМасив();
+            мапа = рядки[1].Trim().Розділ("\n")
+                .Вибір(ряд => ряд.Розділ(" = "))
+                .НаСловник(рядки => рядки[0], рядки => рядки[1][1..^1].Розділ(", "));
         }
+
+        private static ТДжерело Агрегат<ТДжерело>(this IEnumerable<ТДжерело> джерело, Func<ТДжерело, ТДжерело, ТДжерело> вибір) =>
+            джерело.Aggregate(вибір);
+
+        private static IEnumerable<ТВиплід> Вибір<ТДжерело, ТВиплід>
+            (this IEnumerable<ТДжерело> джерело, Func<ТДжерело, ТВиплід> вибір) =>
+                джерело.Select(вибір);
+
+        private static IEnumerable<ТВиплід> Вибір<ТДжерело, ТВиплід>
+            (this IEnumerable<ТДжерело> джерело, Func<ТДжерело, ціле, ТВиплід> вибір) =>
+                джерело.Select(вибір);
+
+        private static ТДжерело[] НаМасив<ТДжерело>
+            (this IEnumerable<ТДжерело> джерело) =>
+                джерело.ToArray();
+
+        private static Dictionary<ТКлюч, ТЗнач> НаСловник<ТДжерело, ТКлюч, ТЗнач>
+            (this IEnumerable<ТДжерело> джерело, Func<ТДжерело, ТКлюч> ключ, Func<ТДжерело, ТЗнач> знач) =>
+                джерело.ToDictionary(ключ, знач);
+
+        private static ціле Індекс<Т>
+            (this Т[] масив, Т знач) =>
+                Array.IndexOf(масив, знач);
+
+        private static ціле Довжина
+            (this Array масив) =>
+                масив.Length;
+
+        private static ціле Кількість<ТДжерело>
+            (this ICollection<ТДжерело> джерело) =>
+                джерело.Count;
+
+        private static Dictionary<ТКлюч, ТЗнач>.KeyCollection Ключі<ТКлюч, ТЗнач>
+            (this Dictionary<ТКлюч, ТЗнач> словник) =>
+                словник.Keys;
+
+        private static Dictionary<ТКлюч, ТЗнач>.ValueCollection Значення<ТКлюч, ТЗнач>
+            (this Dictionary<ТКлюч, ТЗнач> словник) =>
+                словник.Values;
+
+        private static ціле Індекс
+            (this рядок ряд, літера літ) =>
+                ряд.IndexOf(літ);
+
+        private static рядок[] Розділ
+            (this рядок ряд, рядок розд) =>
+                ряд.Split(розд);
     }
 }
