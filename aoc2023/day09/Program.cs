c6faa9e1a1d3e99234df149a1d60c0ff6bbc7e0d
Dmitry Shechtman <damnian@gmail.com>
2023-12-09 09:27:21 +0200

    Add day09

---
+++ aoc2023/day09/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace day09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var values = Parse(args[0]);
+            Console.WriteLine(Part1(values));
+            Console.WriteLine(Part2(values));
+        }
+
+        private static int Part1(int[][] values) =>
+            values.Select(GetDiffs)
+                .Sum(aa => aa.Sum(a => a[^1]));
+
+        private static int Part2(int[][] values) =>
+            values.Select(GetDiffs)
+                .Sum(aa => aa.Reverse().Aggregate(0, (a, v) => v[0] - a));
+
+        private static IEnumerable<int[]> GetDiffs(int[] a)
+        {
+            yield return a;
+            while (a.Any(v => v != 0))
+                yield return a = a[1..].Select((v, i) => v - a[i]).ToArray();
+        }
+
+        private static int[][] Parse(string path) =>
+            File.ReadAllLines(path)
+                .Select(s => s.Split(' ').Select(int.Parse).ToArray())
+                .ToArray();
+    }
+}
