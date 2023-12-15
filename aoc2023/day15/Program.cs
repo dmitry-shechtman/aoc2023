a01f713329f38a129c00603ae73e0e82e9a5395f
Dmitry Shechtman <damnian@gmail.com>
2023-12-15 09:44:07 +0200

    Incremental Update()

--- aoc2023/day15/Program.cs
+++ aoc2023/day15/Program.cs
@@ -18,40 +18,51 @@ namespace aoc.aoc2023.day15
             Console.WriteLine(Part2(tt));
         }
 
-        private static int Part1(List<(string step, string label, int length)> tt) =>
+        private static int Part1(IEnumerable<(string step, string label, int length)> tt) =>
             tt.Sum(t => GetHash(t.step));
 
-        private static int Part2(List<(string step, string label, int length)> tt)
+        private static int Part2(IEnumerable<(string step, string label, int length)> tt)
         {
             var boxes = Enumerable.Range(0, 256)
-                .Select(_ => new List<(string, int length)>())
+                .Select(_ => new List<(string, int)>())
                 .ToArray();
-            tt.ForEach(t => Update(boxes, t.label, t.length));
-            return boxes.Select((b, i) =>
-                b.Select((l, j) =>
-                    (i + 1) * (j + 1) * l.length).Sum())
-                .Sum();
+            return tt.Sum(t => Update(boxes, t.label, t.length));
         }
 
         private static int GetHash(string s) =>
             Encoding.ASCII.GetBytes(s)
                 .Aggregate(0, (a, b) => (a + b) * 17 % 256);
 
-        private static void Update(List<(string label, int)>[] boxes, string label, int length)
+        private static int Update(List<(string label, int length)>[] boxes, string label, int length)
         {
-            var box = boxes[GetHash(label)];
-            var index = box.FindIndex(t => t.label == label);
-            if (index >= 0)
-                box.RemoveAt(index);
+            var boxIndex = GetHash(label);
+            var box = boxes[boxIndex];
+            var lensIndex = box.FindIndex(t => t.label == label);
+            var delta = 0;
+            if (lensIndex >= 0)
+            {
+                delta -= GetDelta(box, boxIndex, lensIndex);
+                box.RemoveAt(lensIndex);
+            }
             if (length > 0)
-                box.Insert(index >= 0 ? index : box.Count, (label, length));
+            {
+                if (lensIndex < 0)
+                    lensIndex = box.Count;
+                box.Insert(lensIndex, (label, length));
+                delta += GetDelta(box, boxIndex, lensIndex);
+            }
+            return delta;
         }
 
-        private static List<(string, string, int)> Parse(string path) =>
+        private static int GetDelta(List<(string label, int length)> box, int boxIndex, int lensIndex) =>
+            (boxIndex + 1) *
+                (box.Skip(lensIndex).Sum(t => t.length) +
+                lensIndex * box[lensIndex].length);
+
+        private static IEnumerable<(string, string, int)> Parse(string path) =>
             File.ReadAllText(path).Trim().Split(',')
                 .Select(s => Regex.Split(s))
-                .Select(ParseOne)
-                .ToList();
+                .Select(ParseOne);
 
         private static (string, string, int) ParseOne(string[] ss) =>
             (ss[1], ss[2], ss[4].Length > 0 ? int.Parse(ss[4]) : 0);
