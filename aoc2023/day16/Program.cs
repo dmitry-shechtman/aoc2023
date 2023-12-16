c69068424d6ff676678546afe7b9911c45962cea
Dmitry Shechtman <damnian@gmail.com>
2023-12-16 13:58:52 +0200

    Optimize day16

--- aoc2023/day16/Program.cs
+++ aoc2023/day16/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace aoc.aoc2023.day16
 {
@@ -8,30 +9,74 @@ namespace aoc.aoc2023.day16
     {
         static void Main(string[] args)
         {
-            var s = File.ReadAllText(args[0]);
-            var r = VectorRange.FromField(s);
-            Console.WriteLine(Part1(s, r));
-            Console.WriteLine(Part2(s, r));
+            string path = args[0];
+            var dd = Parse(path, out var r);
+            Console.WriteLine(Part1(dd));
+            Console.WriteLine(Part2(dd, r));
         }
 
-        private static int Part1(string s, VectorRange r) =>
-            Energize(Vector.Zero, 1, s, new int[r.Count], r);
+        private static int Part1(uint[] dd) =>
+            Energize(1, dd, new int[dd.Length >> 2]);
 
-        private static int Part2(string s, VectorRange r) =>
-            r.Except(new VectorRange((1, 1), r.Max - (1, 1)))
-                .Max(p => Enumerable.Range(0, 4)
-                    .Max(h => Energize(p, h, s, new int[r.Count], r)));
-
-        private static int Energize(Vector p, int h, string s, int[] a, VectorRange r)
+        private static int Part2(uint[] dd, VectorRange r)
         {
-            if (!p.TryGetValue(a, r, out int m) ||
-                p.SetValue(a, m | 1 << h, r) == m)
-                    return 0;
-            int i = "|-./?\\".IndexOf(p.GetChar(s, r));
-            h = i > 1 ? h ^ i - 2 : h;
-            return (m == 0 ? 1 : 0) + (((h & 1) ^ 1) != i
-                ? Energize(p + Vector.Headings[h], h, s, a, r)
-                : Energize(p, i, s, a, r) + Energize(p, i + 2, s, a, r));
+            var data = new[]
+            {
+                (r.Max.y * r.Width, r.Count, 1      ),
+                (0,                 r.Count, r.Width),
+                (0,                 r.Width, 1      ),
+                (r.Max.x,           r.Count, r.Width)
+            };
+            var tasks = new Task<int>[4];
+            for (int i = 0; i < 4; i++)
+            {
+                var (start, end, step) = data[i];
+                tasks[i] = Task.Run(() => Max(start << 2 | i, end << 2, step << 2, dd));
+            }
+            return Task.WhenAll(tasks).Result.Max();
+        }
+
+        private static int Max(int start, int end, int step, uint[] dd)
+        {
+            var nn = new int[(end - start + 1) / step + 1];
+            var a = new int[dd.Length >> 2];
+            for (int i = 0, p = start; p < end; p += step, ++i)
+            {
+                a.Clear();
+                nn[i] = Energize(p, dd, a);
+            }
+            return nn.Max();
+        }
+
+        private static int Energize(int p, uint[] dd, int[] a)
+        {
+            int i = p >> 2, v = a[i], n = v == 0 ? 1 : 0;
+            uint q;
+            if (v != (v |= 1 << (p & 3)))
+                for (a[i] = v, q = dd[p]; q > 0; q >>= 16)
+                    n += Energize((ushort)q, dd, a);
+            return n;
+        }
+
+        private static uint[] Parse(string path, out VectorRange r)
+        {
+            var s = File.ReadAllText(path);
+            r = VectorRange.FromField(s);
+            var dd = new uint[r.Count << 2];
+            Vector p = Vector.Zero;
+            for (int i = 0, j = 0, h; i < s.Length; i++, p = h >= 0 ? p + (1, 0) : (0, p.y + 1))
+                if ((h = "|-./?\\".IndexOf(s[i])) >= 0)
+                    for (int k = 0; k < 4; ++k)
+                        dd[j++] = (k & 1) != (h ^ 1)
+                            ? GetDirection(p, k ^ (h > 1 ? h - 2 : 0), r)
+                            : GetDirection(p, h, r) << 16 | GetDirection(p, h + 2, r);
+            return dd;
+        }
+
+        private static uint GetDirection(Vector p, int i, VectorRange r)
+        {
+            Vector q = p + Vector.Headings[i];
+            return r.IsMatch(q) ? (uint)((q.y * r.Width + q.x) << 2 | i) : 0;
         }
     }
 }
