be4254f8f286a2bd019b63714273203e2968e7d4
Dmitry Shechtman <damnian@gmail.com>
2023-12-20 19:18:40 +0200

    Use long for everything

--- aoc2023/day20/Program.cs
+++ aoc2023/day20/Program.cs
@@ -5,82 +5,106 @@ using System.Linq;
 
 namespace aoc.aoc2023.day20
 {
-    abstract record Module(long Mask, long DestMask)
+    sealed class LongState
     {
-        public abstract bool Transform(long state, bool pulse);
-    }
+        private const int CountsShift = 32;
 
-    record RelayModule(long Mask, long DestMask) : Module(Mask, DestMask)
-    {
-        public override bool Transform(long state, bool pulse) =>
-            pulse;
-    }
+        private long counts;
 
-    record FlipFlopModule(long Mask, long DestMask) : Module(Mask, DestMask)
-    {
-        public override bool Transform(long state, bool pulse) =>
-            (state & Mask) == 0;
-    }
+        public LongState(long flipMask, long conjMask)
+        {
+            FlipMask = flipMask;
+            LcmMask = ConjMask = conjMask;
+            Lcm = 1;
+        }
 
-    record ConjunctionModule(long Mask, long DestMask, long SourceMask) : Module(Mask, DestMask)
-    {
-        public override bool Transform(long state, bool pulse) =>
-            (state & SourceMask) != SourceMask;
+        public long FlipMask { get; }
+        public long ConjMask { get; }
+        public long State { get; set; }
+
+        public long Low
+        {
+            get => (uint)counts;
+            set => counts = counts >> CountsShift << CountsShift | value;
+        }
+
+        public long High
+        {
+            get => counts >> CountsShift;
+            set => counts = value << CountsShift | (uint)counts;
+        }
+
+        public long Product => Low * High;
+
+        public long Lcm     { get; set; }
+        public long LcmMask { get; set; }
     }
 
     class Program
     {
-        private const string StartKey = "broadcaster";
+        private const string StartKey          = "broadcaster";
+        private const char   FlipFlopPrefix    = '%';
+        private const char   ConjunctionPrefix = '&';
 
         static void Main(string[] args)
         {
-            var modules = GetModules(args[0], out int start);
-            var state = 0L;
-            var counts = new int[2];
-            var lengths = CreateLengths(modules);
-            Console.WriteLine(Part1(modules, start, ref state, counts, lengths));
-            Console.WriteLine(Part2(modules, start, ref state, counts, lengths));
+            var modules = GetModules(args[0], out int start, out var state);
+            Console.WriteLine(Part1(modules, start, state));
+            Console.WriteLine(Part2(modules, start, state));
         }
 
-        private static int Part1(Module[] modules, int start, ref long state, int[] counts, long[] lengths)
+        private static long Part1((long, long)[] modules, int start, LongState state)
         {
             for (int i = 1; i <= 1000; i++)
-                Step(modules, start, ref state, counts, lengths, i);
-            return counts.Product();
+                Step(modules, start, state, i);
+            return state.Product;
         }
 
-        private static long Part2(Module[] modules, int start, ref long state, int[] counts, long[] lengths)
+        private static long Part2((long, long)[] modules, int start, LongState state)
         {
-            for (int i = 1001; !lengths.All(v => v > 0); i++)
-                Step(modules, start, ref state, counts, lengths, i);
-            return lengths.Lcm();
+            for (int i = 1001; state.LcmMask > 0; i++)
+                Step(modules, start, state, i);
+            return state.Lcm;
         }
 
-        private static void Step(Module[] modules, int start, ref long state, int[] counts, long[] lengths, int step)
+        private static void Step((long, long)[] modules, int start, LongState state, int step)
         {
             Queue<(bool, int)> queue = new();
             queue.Enqueue((false, start));
             while (queue.TryDequeue(out var tuple))
             {
-                var (pulse, curr) = tuple;
-                ++counts[pulse ? 1 : 0];
-                var module = modules[curr];
-                if (module is FlipFlopModule && pulse)
-                    continue;
-                pulse = module.Transform(state, pulse);
-                state = pulse
-                    ? state | module.Mask
-                    : state & ~module.Mask;
-                if (pulse && lengths[curr] == 0)
-                    lengths[curr] = step;
-                int next = 0;
-                for (var mask = 1L; next < modules.Length; mask <<= 1, ++next)
-                    if ((module.DestMask & mask) != 0)
-                        queue.Enqueue((pulse, next));
+                var (pulse, index) = tuple;
+                if (!pulse)
+                    ++state.Low;
+                else
+                    ++state.High;
+                var mask = 1L << index;
+                var (destMask, srcMask) = modules[index];
+                if ((mask & state.FlipMask) != 0)
+                {
+                    if (pulse)
+                        continue;
+                    pulse = (state.State & mask) == 0;
+                }
+                else if ((mask & state.ConjMask) != 0)
+                {
+                    pulse = (state.State & srcMask) != srcMask;
+                }
+                state.State = pulse
+                    ? state.State | mask
+                    : state.State & ~mask;
+                if ((mask & state.LcmMask) != 0 && pulse)
+                {
+                    state.Lcm = MathEx.Lcm(state.Lcm, step);
+                    state.LcmMask &= ~mask;
+                }
+                for (mask = 1L, index = 0; index < modules.Length; mask <<= 1, ++index)
+                    if ((destMask & mask) != 0)
+                        queue.Enqueue((pulse, index));
             }
         }
 
-        private static Module[] GetModules(string path, out int start)
+        private static (long, long)[] GetModules(string path, out int start, out LongState state)
         {
             var tuples = Parse(path);
             tuples.SelectMany(t => t.dests)
@@ -89,41 +113,43 @@ namespace aoc.aoc2023.day20
                 .ForEach(d => tuples.Add((d, Array.Empty<string>())));
             var keys = tuples.Select(GetKey).ToArray();
             var modules = tuples
-                .Select((t, i) => CreateModule(i, t.key, t.dests, tuples, keys))
+                .Select(t => CreateModule(t.key, t.dests, tuples, keys))
                 .ToArray();
             start = keys.IndexOf(StartKey);
+            var flipMask = GetFlipFlopMask(tuples);
+            var conjMask = GetConjunctionMask(tuples);
+            state = new(flipMask, conjMask);
             return modules;
         }
 
-        private static long[] CreateLengths(Module[] modules) =>
-            modules
-                .Select(m => m is ConjunctionModule ? 0L : 1L)
-                .ToArray();
-
-        private static Module CreateModule(int index, string key, string[] dests, List<(string key, string[] dests)> tuples, string[] keys) =>
-            CreateModule(1L << index, key, GetDestMask(dests, keys), tuples);
-
-        private static Module CreateModule(long mask, string key, long destMask, List<(string key, string[] dests)> tuples) => key[0] switch
-        {
-            '%' => new FlipFlopModule(mask, destMask),
-            '&' => new ConjunctionModule(mask, destMask, GetSourceMask(GetKey(key), tuples)),
-            _ => new RelayModule(mask, destMask),
-        };
+        private static (long, long) CreateModule(string key, string[] dests, List<(string key, string[] dests)> tuples, string[] keys) =>
+            (GetDestMask(dests, keys), GetSourceMask(GetKey(key), tuples));
 
         private static string GetKey((string key, string[] dests) tuple) =>
             GetKey(tuple.key);
 
         private static string GetKey(string key) =>
-            key.TrimStart('%', '&');
+            key.TrimStart(FlipFlopPrefix, ConjunctionPrefix);
 
         private static long GetDestMask(string[] dests, string[] keys) =>
-            dests.Select(keys.IndexOf).Sum(index => 1L << index);
+            dests.Select(keys.IndexOf)
+                .Sum(index => 1L << index);
 
         private static long GetSourceMask(string key, List<(string key, string[] dests)> tuples) =>
             tuples.Select()
                 .Where(t => t.Value.dests.Contains(key))
-                .Select(t => 1L << t.Index)
-                .Sum();
+                .Sum(t => 1L << t.Index);
+
+        private static long GetFlipFlopMask(List<(string key, string[] dests)> tuples) =>
+            GetMask(FlipFlopPrefix, tuples);
+
+        private static long GetConjunctionMask(List<(string key, string[] dests)> tuples) =>
+            GetMask(ConjunctionPrefix, tuples);
+
+        private static long GetMask(char c, List<(string key, string[] dests)> tuples) =>
+            tuples.Select()
+                .Where(t => t.Value.key[0] == c)
+                .Sum(t => 1L << t.Index);
 
         private static List<(string key, string[] dests)> Parse(string path) =>
             File.ReadAllLines(path)
