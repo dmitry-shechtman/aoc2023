08cfcdf461a09443ea6ab7d7ee40a38663ded6b2
Dmitry Shechtman <damnian@gmail.com>
2023-12-23 18:19:05 +0200

    Use Graph<bool>

--- aoc2023/day22/Program.cs
+++ aoc2023/day22/Program.cs
@@ -17,10 +17,10 @@ namespace aoc.aoc2023.day22
             Console.WriteLine(Part2(graph));
         }
 
-        private static int Part1(Graph graph) =>
+        private static int Part1(Graph<bool> graph) =>
             graph.Count(i => graph.All(j => CountSupports(graph, i, j) != 1));
 
-        private static int Part2(Graph graph) =>
+        private static int Part2(Graph<bool> graph) =>
             graph.Sum(i => CountSupports(new(graph), i));
 
         private static void Drop(Brick[] bricks)
@@ -29,20 +29,20 @@ namespace aoc.aoc2023.day22
                 bricks[i] -= (0, 0, GetHeight(bricks[i], bricks));
         }
 
-        private static Graph CreateGraph(Brick[] bricks) =>
+        private static Graph<bool> CreateGraph(Brick[] bricks) =>
             new((i, j) => Supports(bricks[i], bricks[j]), bricks.Length);
 
         private static int GetHeight(Brick brick, Brick[] bricks) =>
             bricks.Where(b => Below(b, brick))
                 .TryMin(b => brick.Min.z - b.Max.z, brick.Min.z) - 1;
 
-        private static int CountSupports(Graph graph, int i) =>
-            graph.Outgoing[i]
+        private static int CountSupports(Graph<bool> graph, int i) =>
+            graph.Outgoing[i].Keys
                 .Where(j => graph.Remove(i, j) && graph.Incoming[j].Count == 0)
                 .Sum(j => 1 + CountSupports(graph, j));
 
-        private static int CountSupports(Graph graph, int i, int j) =>
-            graph.Outgoing[i].Contains(j)
+        private static int CountSupports(Graph<bool> graph, int i, int j) =>
+            graph.Outgoing[i].ContainsKey(j)
                 ? graph.Incoming[j].Count
                 : -1;
 
