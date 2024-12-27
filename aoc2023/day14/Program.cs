b27c621d127384c5a6d1b90de21f792a5e40035d
Dmitry Shechtman <damnian@gmail.com>
2024-12-27 16:26:53 +0200

    Simplify day14

--- aoc2023/day14/Program.cs
+++ aoc2023/day14/Program.cs
@@ -1,14 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Text;
 
 namespace aoc.aoc2023.day14
 {
     class Program
     {
-        private const byte Empty = 0x2E, Round = 0x4F, Square = 0x23, NewLine = 0x0A;
-
         static void Main(string[] args)
         {
             string s = File.ReadAllText(args[0]);
@@ -18,23 +15,36 @@ namespace aoc.aoc2023.day14
 
         private static int Part1(string s)
         {
-            int width1 = s.IndexOf('\n') + 1;
-            var r = Encoding.ASCII.GetBytes(s);
-            TiltNorth(r, width1);
-            return GetLoad(Encoding.ASCII.GetString(r), width1);
+            Dish dish = new(s);
+            var r = dish.TiltNorth();
+            return dish.GetLoad(new(r));
         }
 
         private static int Part2(string s)
         {
-            int width1 = s.IndexOf('\n') + 1;
+            Dish dish = new(s);
             List<string> history = new();
             int i;
-            for (var r = Encoding.ASCII.GetBytes(s); (i = history.IndexOf(s)) < 0; s = GetNext(r, width1))
+            for (; (i = history.IndexOf(s)) < 0; s = dish.GetNext())
                 history.Add(s);
-            return GetLoad(history[(1000000000 - i) % (history.Count - i) + i], width1);
+            return dish.GetLoad(history[(1000000000 - i) % (history.Count - i) + i]);
+        }
+    }
+
+    class Dish
+    {
+        private const char Empty = '.', Round = 'O', Square = '#', NewLine = '\n';
+
+        private readonly char[] r;
+        private readonly int width1;
+
+        public Dish(string input)
+        {
+            r = input.ToCharArray();
+            width1 = input.IndexOf('\n') + 1;
         }
 
-        private static int GetLoad(string s, int width1)
+        public int GetLoad(string s)
         {
             int load = 0;
             for (int i = 0; i < s.Length; i++)
@@ -43,82 +53,43 @@ namespace aoc.aoc2023.day14
             return load;
         }
 
-        private static string GetNext(byte[] r, int width1)
+        public string GetNext()
         {
-            TiltNorth(r, width1);
-            TiltWest (r);
-            TiltSouth(r, width1);
-            TiltEast (r);
-            return Encoding.ASCII.GetString(r);
+            TiltNorth();
+            TiltWest();
+            TiltSouth();
+            TiltEast();
+            return new(r);
         }
 
-        private static void TiltNorth(byte[] r, int width1)
+        public char[] TiltNorth() =>
+            Tilt(0, 1, 0, width1);
+
+        private void TiltWest() =>
+            Tilt(0, 1);
+
+        private void TiltSouth() =>
+            Tilt(width1 - 2, -1, r.Length - width1, -width1);
+
+        private void TiltEast() =>
+            Tilt(r.Length - 1, -1);
+
+        private char[] Tilt(int start, int step, int offset, int step2)
         {
-            for (int x = 0; x < width1 - 1; ++x)
-            {
-                for (int i = x, j = i - width1; i < r.Length; i += width1)
-                {
-                    switch (r[i])
-                    {
-                        case Round:
-                            r[i] = Empty;
-                            r[j += width1] = Round;
-                            break;
-                        case Square:
-                            j = i;
-                            break;
-                    }
-                }
-            }
+            for (int i = start; i >= 0 && i < width1 - 1; i += step)
+                Tilt(i + offset, step2);
+            return r;
         }
 
-        private static void TiltWest(byte[] r)
+        private void Tilt(int start, int step)
         {
-            for (int i = 0, j = -1; i < r.Length; ++i)
-            {
-                switch (r[i])
-                {
-                    case Round:
-                        r[i] = Empty;
-                        r[++j] = Round;
-                        break;
-                    case Square:
-                    case NewLine:
-                        j = i;
-                        break;
-                }
-            }
-        }
-
-        private static void TiltSouth(byte[] r, int width1)
-        {
-            for (int x = width1 - 2; x >= 0; --x)
-            {
-                for (int j = r.Length + x, i = j - width1; i >= 0; i -= width1)
-                {
-                    switch (r[i])
-                    {
-                        case Round:
-                            r[i] = Empty;
-                            r[j -= width1] = Round;
-                            break;
-                        case Square:
-                            j = i;
-                            break;
-                    }
-                }
-            }
-        }
-
-        private static void TiltEast(byte[] r)
-        {
-            for (int j = r.Length, i = j - 1; i >= 0; --i)
+            for (int i = start, j = i - step; i >= 0 && i < r.Length; i += step)
             {
                 switch (r[i])
                 {
                     case Round:
                         r[i] = Empty;
-                        r[--j] = Round;
+                        r[j += step] = Round;
                         break;
                     case Square:
                     case NewLine:
