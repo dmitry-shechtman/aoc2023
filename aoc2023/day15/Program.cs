a0243b6cc4092dcf6608bcdeaefc6ba8c8f6e959
Dmitry Shechtman <damnian@gmail.com>
2025-01-13 14:35:32 +0200

    Streamline day15

--- aoc2023/day15/Program.cs
+++ aoc2023/day15/Program.cs
@@ -9,54 +9,58 @@ namespace aoc.aoc2023.day15
 {
     class Program
     {
-        private static readonly Regex Regex = new(@"^(([a-z]+)(-|=([1-9])))$");
-
         static void Main(string[] args)
         {
-            var tt = Parse(args[0]);
+            var tt = Item.ParseAll(args[0]);
             Console.WriteLine(Part1(tt));
             Console.WriteLine(Part2(tt));
         }
 
-        private static int Part1(List<(string step, string label, int length)> tt) =>
-            tt.Sum(t => GetHash(t.step));
+        private static int Part1(List<Item> tt) =>
+            tt.Sum(t => GetHash(t.Step));
 
-        private static int Part2(List<(string step, string label, int length)> tt)
+        private static int Part2(List<Item> tt)
         {
             var boxes = Enumerable.Range(0, 256)
-                .Select(_ => new LinkedList<(string, int length)>())
+                .Select(_ => new LinkedList<Slot>())
                 .ToArray();
-            tt.ForEach(t => Update(boxes, t.label, t.length));
-            return boxes.Select((b, i) =>
-                b.Select((l, j) =>
-                    (i + 1) * (j + 1) * l.length).Sum())
-                .Sum();
+            tt.ForEach(t => t.Update(boxes[GetHash(t.Value.Label)]));
+            return boxes.Sum((b, i) =>
+                b.Sum((l, j) =>
+                    (i + 1) * (j + 1) * l.Length));
         }
 
         private static int GetHash(string s) =>
             Encoding.ASCII.GetBytes(s)
-                .Aggregate(0, (a, b) => (a + b) * 17 % 256);
+                .Aggregate(0, (a, b) => (byte)((a + b) * 17));
+    }
 
-        private static void Update(LinkedList<(string label, int)>[] boxes, string label, int length)
+    readonly record struct Slot(string Label, int Length);
+
+    readonly record struct Item(string Step, Slot Value)
+    {
+        private static readonly Regex Regex = new(@"^(([a-z]+)(-|=([1-9])))$");
+
+        public void Update(LinkedList<Slot> box)
         {
-            var box = boxes[GetHash(label)];
-            var node = box.Find(t => t.label == label);
+            var (label, length) = Value;
+            var node = box.Find(t => t.Label == label);
             if (node is not null)
                 if (length == 0)
                     box.Remove(node);
                 else
-                    node.Value = (label, length);
+                    node.Value = Value;
             else if (length > 0)
-                box.AddLast((label, length));
+                box.AddLast(Value);
         }
 
-        private static List<(string, string, int)> Parse(string path) =>
+        public static List<Item> ParseAll(string path) =>
             File.ReadAllText(path).Trim().Split(',')
-                .Select(s => Regex.Split(s))
+                .Select1(Regex.Split)
                 .Select(ParseOne)
                 .ToList();
 
-        private static (string, string, int) ParseOne(string[] ss) =>
-            (ss[1], ss[2], ss[4].Length > 0 ? int.Parse(ss[4]) : 0);
+        public static Item ParseOne(string[] ss) =>
+            new(ss[1], new(ss[2], ss[4].Length > 0 ? int.Parse(ss[4]) : 0));
     }
 }
